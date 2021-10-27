30008bbb28e2e5458f81e4dd871aaf4102265905
RamezesDong <donghouze666@outlook.com>
2021-10-27 14:44:54 +0800

    3

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,124 +1,135 @@
-public class ArrayDeque <T>{
+public class ArrayDeque<T> {
     private T[] items;
     private int size;
     private int nextFirst;
     private int nextLast;
 
-    public ArrayDeque(){
-        items = (T[])new Object[8];
-        size =0;
-        nextFirst=4;
-        nextLast=5;
+    public ArrayDeque() {
+        items = (T[]) new Object[8];
+        size = 0;
+        nextFirst = 4;
+        nextLast = 5;
     }
 
     /**
-     *my solustion is read from first to last and push in [0,size]
+     * my solustion is read from first to last and push in [0,size]
      */
-    private void resize (int capacity){
-        T[] a=(T [])new Object[capacity];
-        int f2=nextFirst;
-        for(int i=0;i<size;i++){
-            if(f2+1==items.length){
-                f2=0;
+    @SuppressWarnings("checkstyle:WhitespaceAround")
+    private void resize(int capacity) {
+        T[] a = (T[]) new Object[capacity];
+        int f2 = nextFirst;
+        for (int i = 0; i < size; i++) {
+            if (f2 + 1 == items.length) {
+                f2 = 0;
+            } else {
+                f2 = f2 + 1;
             }
-            else
-            f2=f2+1;
-            a[i]=items[f2];
+            a[i] = items[f2];
         }
-        nextLast = size ;
-        nextFirst = capacity-1;
-        items =a;
+        nextLast = size;
+        nextFirst = capacity - 1;
+        items = a;
 
     }
-    private void checkresize(){
-        if(size< items.length/4&&items.length>=16){
-            int size2=items.length/2;
+
+    private void checkresize() {
+        if (size < items.length / 4 && items.length >= 16) {
+            int size2 = items.length / 2;
             resize(size2);
+        } else {
+            return;
         }
-        else return;
     }
-    public  void addFirst (T item) {
-        if(size==items.length)
-            resize(size*2);
-        items[nextFirst]=item;
-        if(nextFirst == 0)
-        {
-            nextFirst=items.length-1;
+
+    public void addFirst(T item) {
+        if (size == items.length) {
+            resize(size * 2);
         }
-        else
-            nextFirst=nextFirst-1;
-        size =size +1;
+        items[nextFirst] = item;
+        if (nextFirst == 0) {
+            nextFirst = items.length - 1;
+        } else {
+            nextFirst = nextFirst - 1;
+        }
+        size = size + 1;
     }
 
     public void addLast(T item) {
-        if(size==items.length)
-            resize(size*2);
-        items[nextLast]=item;
-        if(nextLast == items.length - 1)
-        {
-            nextLast=0;
+        if (size == items.length) {
+            resize(size * 2);
         }
-        else
-            nextLast=nextLast+1;
-        size =size +1;
+        items[nextLast] = item;
+        if (nextLast == items.length - 1) {
+            nextLast = 0;
+        } else {
+            nextLast = nextLast + 1;
+        }
+        size = size + 1;
     }
 
-    public boolean isEmpty(){
-        if(size==0)
+    public boolean isEmpty() {
+        if (size == 0) {
             return true;
+        }
         return false;
     }
 
-    public int size (){
+    public int size() {
         return size;
     }
 
     public void printDeque() {
-        int fr=nextFirst;
-        for(int i=0;i<size;i++){
-            if(fr+1 == items.length)
-                fr=0;
-            else
-                fr =fr +1;
-            System.out.print(items[fr]+" ");
+        int fr = nextFirst;
+        for (int i = 0; i < size; i++) {
+            if (fr + 1 == items.length) {
+                fr = 0;
+            } else {
+                fr = fr + 1;
+            }
+            System.out.print(items[fr] + " ");
         }
     }
 
-    public T removeFirst(){
-        if(size==0)
+    public T removeFirst() {
+        if (size == 0) {
             return null;
-        T re;
-        if(nextFirst==items.length-1){
-            nextFirst=0;
         }
-        else nextFirst=nextFirst+1;
-        re=items[nextFirst];
-        items[nextFirst]=null;
-        size=size-1;
+        T re;
+        if (nextFirst == items.length - 1) {
+            nextFirst = 0;
+        } else {
+            nextFirst = nextFirst + 1;
+        }
+        re = items[nextFirst];
+        items[nextFirst] = null;
+        size = size - 1;
         checkresize();
         return re;
     }
 
-    public T removeLast(){
-        if(size==0)
+    public T removeLast() {
+        if (size == 0) {
             return null;
-        T re;
-        if(nextLast==0){
-            nextLast=items.length-1;
         }
-        else nextLast=nextLast-1;
-        re=items[nextLast];
-        items[nextLast]=null;
-        size=size-1;
+        T re;
+        if (nextLast == 0) {
+            nextLast = items.length - 1;
+        } else {
+            nextLast = nextLast - 1;
+        }
+        re = items[nextLast];
+        items[nextLast] = null;
+        size = size - 1;
         checkresize();
         return re;
     }
 
-    public T get(int index){
-        int fr= nextFirst+1;
-        fr = fr +index;
-        if(fr>= items.length)
-            fr=fr-items.length;
+    public T get(int index) {
+        int fr = nextFirst + 1;
+        fr = fr + index;
+        if (fr >= items.length) {
+            fr = fr - items.length;
+        }
         return items[fr];
     }
 
--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,24 +1,25 @@
-import java.util.Iterator;
-
-public class LinkedListDeque <T>{
-    private class ListDeque  {
+public class LinkedListDeque<T> {
+    private class ListDeque {
         private T item;
         private ListDeque next;
         private ListDeque last;
-        private ListDeque(T i, ListDeque n,ListDeque l){
-            item=i;
-            next=n;
-            last=l;
+
+        private ListDeque(T i, ListDeque n, ListDeque l) {
+            item = i;
+            next = n;
+            last = l;
         }
     }
+
     private int size;
     private ListDeque first;
     private ListDeque last;
+
     //    private LinkedListDeque first;
-    public LinkedListDeque(){
+    public LinkedListDeque() {
         first = null;
         last = null;
-        size=0;
+        size = 0;
     }
 //    public LinkedListDeque(T x){
 //        first = new ListDeque(x,null,null);
@@ -31,93 +32,104 @@ public class LinkedListDeque <T>{
 //        size = 1;
 //    }
 
-    public void addFirst(T x){
-        first = new ListDeque(x,first,null);
-        if(first.next!=null)
-        first.next.last=first;
-        size = size +1;
-        if(size ==1)
-            last=first;
+    public void addFirst(T x) {
+        first = new ListDeque(x, first, null);
+        if (first.next != null) {
+            first.next.last = first;
+        }
+        size = size + 1;
+        if (size == 1) {
+            last = first;
+        }
     }
 
     public void addLast(T x) {
-        last = new ListDeque(x,null,last);
-        if(last.last!=null)
-        last.last.next=last;
+        last = new ListDeque(x, null, last);
+        if (last.last != null) {
+            last.last.next = last;
+        }
         size = size + 1;
-        if(size==1)
-            first=last;
+        if (size == 1) {
+            first = last;
+        }
     }
-    public boolean isEmpty (){
-        if(size == 0)
+
+    public boolean isEmpty() {
+        if (size == 0) {
             return true;
+        }
         return false;
     }
-    public int size(){
+
+    public int size() {
         return size;
     }
 
     public void printDeque() {
-        ListDeque p=first;
-        while(p!=null){
-            System.out.print(p.item+" ");
-            p=p.next;
+        ListDeque p = first;
+        while (p != null) {
+            System.out.print(p.item + " ");
+            p = p.next;
         }
     }
 
-    public T removeFirst(){
-        if(size==0)
+    public T removeFirst() {
+        if (size == 0) {
             return null;
-        T fr= first.item;
-        if(size==1) {
-            first=null;
-            last=null;
         }
-        else{
-            first =first.next;
-            first.last=null;
+        T fr = first.item;
+        if (size == 1) {
+            first = null;
+            last = null;
+        } else {
+            first = first.next;
+            first.last = null;
         }
-        size=size-1;
+        size = size - 1;
         return fr;
     }
 
-    public T removeLast(){
-        if(size==0)
+    public T removeLast() {
+        if (size == 0) {
             return null;
-        T la= last.item;
-        if(size==1) {
-            first=null;
-            last=null;
         }
-        else{
-            last =last.last;
-            last.next=null;
+        T la = last.item;
+        if (size == 1) {
+            first = null;
+            last = null;
+        } else {
+            last = last.last;
+            last.next = null;
         }
-        size=size-1;
+        size = size - 1;
         return la;
     }
-    public T get (int index){
-        ListDeque p= first;
-        if(index+1>size)
+
+    public T get(int index) {
+        ListDeque p = first;
+        if (index + 1 > size) {
             return null;
-        int i=0;
-        while (i != index){
-            p=p.next;
+        }
+        int i = 0;
+        while (i != index) {
+            p = p.next;
             i = i + 1;
         }
         return p.item;
     }
 
-    public T getRecursive(int index){
-        if(index+1> size)
+    public T getRecursive(int index) {
+        if (index + 1 > size) {
             return null;
-        return helpgetRecursive(0,index,first);
+        }
+        return helpgetRecursive(0, index, first);
     }
 
-    private T helpgetRecursive(int nw ,int index, ListDeque tm){
-        if(nw==index){
+    private T helpgetRecursive(int nw, int index, ListDeque tm) {
+        if (nw == index) {
             return tm.item;
+        } else {
+            return helpgetRecursive(nw + 1, index, tm.next);
         }
-        else return helpgetRecursive(nw+1,index,tm.next);
     }
 }
